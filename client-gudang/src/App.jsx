c7f45c3d4ead937c55404d48d3f5ff4099ee9995
amarmaruf6800-collab <amarmaruf6800@gmail.com>
2025-12-08 03:21:10 +0700

    Fix endpoint url

--- client-gudang/src/App.jsx
+++ client-gudang/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   // Kalau null = Mode Tambah. Kalau ada angka = Mode Edit.
   const [editId, setEditId] = useState(null);
 
-  const endpoint = "belajar-backend-nu.vercel.app/";
+  const endpoint = "https://belajar-backend-nu.vercel.app/barang";
 
   // 1. FUNGSI AMBIL DATA (Sama kayak sebelumnya)
   async function ambilData() {
